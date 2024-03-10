25e6d3523a2e8e7fdefa1e4678ec20b83820798b
Renato Almeida <renatoam@ymail.com>
2024-03-09 23:45:36 -0300

    feat: add examples of problems of not using types

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+//@ts-check
 const frase = "Front End";
-const total = 100.05
+const total = 100.05;
 
-const empresas = ['Apple','Microsoft']
+const empresas = ["Apple", "Microsoft"];
 
 console.log(frase.toLowerCase());
 //console.log(total.toLowerCase());
 
-// o erro acontece em execução. No typescript não é possível acionar o 
+// o erro acontece em execução. No typescript não é possível acionar o
 // toLowerCase() para um numero
 
 const t = total.toFixed();
 
-console.log(t + 10) // concatenou um numero com uma string
+console.log(t + 10); // concatenou um numero com uma string
 // no typescript isso não ocorreria por ter definido o tipo
 
-
-empresas.map(empresa => empresa.toLowerCase())
+empresas.map((empresa) => empresa.toLowerCase());
 
 const body = document.body;
+const button = document.querySelector("button");
 
-body.style.background = '#000'
+if (button) button.click();
+body.style.background = "#000";
