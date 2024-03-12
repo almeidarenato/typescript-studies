76a97a4df43841ddad575d03416a83d4ae4509c1
Renato Almeida <renatoam@ymail.com>
2024-03-12 13:21:01 -0300

    feat: add async await examples

---
+++ asyncawait/asyncawait.ts
@@ -0,0 +1,21 @@
+const espera1 = fetch('./asyncawait.json');
+
+const espera2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Teste da promise 2")
+    },2000) // apenas com a Promise o await aguarda o "resolve". 
+    
+})
+
+const espera4 = async ():Promise<string> => {
+    return "Teste"
+}
+
+const resultadoFinal = async ()=> {
+    const resultado1 = await(await espera1).json();
+    console.log("dados do json:",resultado1)
+    const resultado2 = await espera2;
+    const resultado4 = await espera4();
+    console.log( `${resultado2} e ${resultado4}`)
+}
+resultadoFinal()
