e42261e183c9b3171419f48b160a9b425ab80414
Renato Almeida <renatoam@ymail.com>
2024-03-16 00:02:43 -0300

    feat: update data from courses

--- exercicio5api/script.js
+++ exercicio5api/script.js
@@ -7,7 +7,13 @@ const buscaCursos = async () => {
 function showCursos(dados) {
     dados.map(dado => {
         document.body.innerHTML += `
-    <div>${dado.nome}</div>
+    <div>${dado.nivel == 'iniciante' ? "<span style='color:blue'>" : "<span style='color:red'>"}  ${dado.nome}</span></div>
+    <div>Horas: ${dado.horas}</div>
+    <div>Tags: <strong> ${dado.tags.map(tag => tag)}</strong></div>
+    <div>Aulas: <strong> ${dado.idAulas.map(idAula => idAula)}</strong></div>
+    <div>Gratuítuo: <strong>${dado.gratuito == true ? "sim" : "não"}</strong></div>
+    <div>Nível: ${dado.nivel}</div>
+    </br>
     
     `;
     });
