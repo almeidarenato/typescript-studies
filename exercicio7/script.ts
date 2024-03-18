e952713e5a1fd2656f22544998625d2cde30003b
Renato Almeida <renatoam@ymail.com>
2024-03-17 23:47:46 -0300

    feat: add exercise using instanceof and explaning prototypes

---
+++ exercicio7/script.ts
@@ -0,0 +1,7 @@
+const linkElement: HTMLElement | null = document.getElementById("origamid");
+if (linkElement) {
+  console.dir(linkElement); // retorna como objeto ao invés de elemento do dom
+  // prototype = classe utilizada como construtor ou extendida para criar o objeto
+  if (linkElement instanceof HTMLAnchorElement)
+    linkElement.href = linkElement.href.replace("http:", "https");
+}
