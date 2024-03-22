6b6c3cdc713c2331e9d78aea6e8cb498dd285426
Renato Almeida <renatoam@ymail.com>
2024-03-21 21:03:54 -0300

    feat: add instanceof to show examples

--- exercicio6classes/script.js
+++ exercicio6classes/script.js
@@ -1,38 +1,34 @@
 "use strict";
 class Produto {
     nome; // defino a variável da classe
-    preco;
-    constructor(nome, preco) {
+    constructor(nome) {
         this.nome = nome;
-        this.preco = preco;
-    }
-    precoReal() {
-        return `R$ ${this.preco}`;
     }
 }
-const livro = new Produto('A guerra dos tronos', 200);
+const livro = new Produto('A guerra dos tronos');
 console.log(livro);
-console.log(livro.precoReal());
 console.log(livro instanceof Produto); // true pois é uma instancia de Produto
 console.log(livro instanceof Array); //false
-class Livro {
+class Livro extends Produto {
     autor;
-    constructor(autor) {
+    constructor(nome, autor) {
+        super(nome);
         this.autor = autor;
     }
 }
-class Jogo {
+class Jogo extends Produto {
     jogadores;
-    constructor(jogadores) {
+    constructor(nome, jogadores) {
+        super(nome);
         this.jogadores = jogadores;
     }
 }
 function buscarProduto(busca) {
     if (busca == 'O Hobbit') {
-        return new Livro('J. R. R. Tolkien');
+        return new Livro('O Hobbit', 'J. R. R. Tolkien');
     }
     if (busca == 'Dark Souls') {
-        return new Jogo(1);
+        return new Jogo('Dark Souls', 1);
     }
     return null;
 }
@@ -41,3 +37,5 @@ if (novoProduto instanceof Livro)
     console.log(novoProduto.autor); // após a verificação do instanceof o TS já aplica o autocomplete correto 
 if (novoProduto instanceof Jogo)
     console.log(novoProduto.jogadores);
+if (novoProduto instanceof Produto)
+    console.log(novoProduto.nome); // mostra apenas a propriedade dentro de Produto
