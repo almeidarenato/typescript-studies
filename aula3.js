b52aef235282101a11131d1c31b0cc99c319054d
Renato Almeida <renatoam@ymail.com>
2024-03-22 22:56:25 -0300

    feat: updates aula3

---
+++ aula3.js
@@ -0,0 +1,6 @@
+"use strict";
+// exercicio 1 - corrigir erros no javascript
+function normalizarTexto(text) {
+    return text.trim().toLowerCase();
+}
+console.log(normalizarTexto("Teste de Texto"));
--- aula3.ts
+++ aula3.ts
@@ -1,3 +1,4 @@
+// exercicio 1 - corrigir erros no javascript
 function normalizarTexto(text: string){
     return text.trim().toLowerCase();
 }
