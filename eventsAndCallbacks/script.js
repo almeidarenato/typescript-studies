7f0ddb26c220ca07d459db9fb46369c6107c9d61
Renato Almeida <renatoam@ymail.com>
2024-03-26 00:05:32 -0300

    feat: add use of this in typescript

--- eventsAndCallbacks/script.js
+++ eventsAndCallbacks/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const buttonClick = document.querySelector('button');
+const buttonClick = document.querySelector('.old');
 function handleClick(event) {
     console.log(event.pageX);
 }
@@ -15,3 +15,14 @@ function handleScroll(event) {
     console.log(event);
 }
 window.addEventListener('scroll', handleScroll);
+// uso do this
+function handleClickNew(event) {
+    console.log(this); // necessário identificar o tipo do this para não gerar erro
+    // o this pode ser o proprio objeto que chamou (onde ele foi chamado)
+    // em outros momentos o this pode estar undefined
+    // outra forma de lidar é console.log(event.currentTarget)
+    // Instancias do tipo EventTarget não permitem interação do dom. 
+    //É necessário usar o instanceof e identificar com o elemento desejado.
+}
+const buttonExample = document.querySelector('.new');
+buttonExample?.addEventListener("click", handleClickNew);
--- eventsAndCallbacks/script.ts
+++ eventsAndCallbacks/script.ts
@@ -1,4 +1,4 @@
-const buttonClick = document.querySelector('button')
+const buttonClick: HTMLButtonElement | null = document.querySelector('.old')
 
 function handleClick(event: PointerEvent){
 console.log(event.pageX)
@@ -20,3 +20,19 @@ function handleScroll(event: Event){
     console.log(event)
 }
 window.addEventListener('scroll',handleScroll)
+
+
+// uso do this
+
+
+function handleClickNew(this: HTMLButtonElement, event: MouseEvent){
+    console.log(this) // necessário identificar o tipo do this para não gerar erro
+    // o this pode ser o proprio objeto que chamou (onde ele foi chamado)
+    // em outros momentos o this pode estar undefined
+    // outra forma de lidar é console.log(event.currentTarget)
+    // Instancias do tipo EventTarget não permitem interação do dom. 
+    //É necessário usar o instanceof e identificar com o elemento desejado.
+}
+const buttonExample: HTMLButtonElement | null = document.querySelector('.new')
+
+buttonExample?.addEventListener("click",handleClickNew)
